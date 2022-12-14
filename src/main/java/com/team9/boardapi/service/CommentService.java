a87ca665155ca0cabe78f81bc3a79421a06aca4c
sungwon <aogn07@naver.com>
2022-12-14 20:26:10 +0900

    [feat] 댓글 수정 조건 부여

--- src/main/java/com/team9/boardapi/service/CommentService.java
+++ src/main/java/com/team9/boardapi/service/CommentService.java
@@ -4,6 +4,7 @@ import com.team9.boardapi.dto.CommentRequestDto;
 import com.team9.boardapi.dto.CommentResponseDto;
 import com.team9.boardapi.entity.Comment;
 import com.team9.boardapi.entity.User;
+import com.team9.boardapi.entity.UserRoleEnum;
 import com.team9.boardapi.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -32,9 +33,17 @@ public class CommentService {
     //댓글 수정
     @Transactional
     public ResponseEntity<CommentResponseDto> updateComment(Long id, CommentRequestDto commentRequestDto, User user) {
-        Comment comment = commentRepository.findByIdAndUserId(id,user.getId()).orElseThrow(
+        Comment comment = commentRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("선택한 댓글을 찾을 수 없습니다")
         );
+
+        //사용자 권한에 따른 댓글 수정
+        UserRoleEnum userRoleEnum = user.getRole();
+
+        //댓글을 작성한 사용자와 동일한 사용자가 아닐 때, 권한이 관리자가 아닐 때
+        if(userRoleEnum != UserRoleEnum.ADMIN || !comment.getUser().getId().equals(user.getId())) {
+            throw new IllegalArgumentException("수정을 할 수 없습니다.");
+        }
 
         comment.update(commentRequestDto);
 
